df57bf02fab1b1a60dc352b9a8434d3d4eb6b489
sayman713 <sayman713@yandex.ru>
2020-05-11 02:22:50 +0300

    optimized the code

--- exchange.py
+++ exchange.py
@@ -2,55 +2,39 @@
 import requests
 import datetime
 from datetime import timedelta
-from os import path
 
 # Автор скрипта: sayman713
-#
 # для вывода данных из скрипта в конфиг conky, необходимо прописать путь до temp,
 # к примеру ${execi 10 cat $HOME/.conky/sayman/temp}
 
 datet = datetime.datetime.now()-timedelta(days=1)
-
 date = datetime.datetime.now()
 
 lincy='http://www.cbr.ru/scripts/XML_daily.asp?date_req='+str(datet.strftime("%d-%m-%Y"))
 lincnow='http://www.cbr.ru/scripts/XML_daily.asp'
-s = requests.post(lincy).text
+st = requests.post(lincy).text
 sn = requests.post(lincnow).text
 
-l = len(s)
-integ = []
-i = 0
-while i < l:
-    s_int = ''
-    a = s[i]
-    while '0' <= a <= '9':
-        s_int += a
-        i += 1
-        if i < l:
-            a = s[i]
-        else:
-            break
-    i += 1
-    if s_int != '':
-        integ.append(s_int)
 
-l = len(sn)
-integn = []
-i = 0
-while i < l:
-    s_int = ''
-    a = sn[i]
-    while '0' <= a <= '9':
-        s_int += a
+def str_num(x):
+    l = len(x)
+    integ = []
+    i = 0
+    while i < l:
+        s_int = ''
+        a = x[i]
+        while '0' <= a <= '9':
+            s_int += a
+            i += 1
+            if i < l:
+                a = x[i]
+            else:
+                break
         i += 1
-        if i < l:
-            a = sn[i]
-        else:
-            break
-    i += 1
-    if s_int != '':
-        integn.append(s_int)
+        if s_int != '':
+            integ.append(s_int)
+    return integ
+
 
 # Индексы валют, подставьте нужный индекс, по умолчанию выведено EUR, USD
 #GBP[19:21] AZN[14:16] AUD[9:11] INR[69:71] KZT[74:76]
@@ -61,17 +45,19 @@ while i < l:
 #TMT[134:136] UZS[139:141] UAH[144:146] CZK[149:151] SEK[154:156]
 #CHF[159:161] ZAR[164:166] KRW[169:171] JPY[174:176]
 
-numu1, numu2 = integ[59:61]
+
+numu1, numu2 = str_num(st)[59:61]
 usd=numu1+'.'+numu2
 flusdt=float(usd)
 
-nume1, nume2 = integ[64:66]
+nume1, nume2 = str_num(st)[64:66]
 eur=nume1+'.'+nume2
 fleurt=float(eur)
 
+
 temp = open('temp', 'w')
 
-numu1, numu2 = integn[59:61]
+numu1, numu2 = str_num(sn)[59:61]
 usd=numu1+'.'+numu2
 flusd=float(usd)
 if flusdt <= flusd:
@@ -79,7 +65,7 @@ if flusdt <= flusd:
 else:
     print(date.strftime(" %d/%m/%Y"), ' USD ',flusd,' ▼', file=temp)
 
-nume1, nume2 = integn[64:66]
+nume1, nume2 = str_num(sn)[64:66]
 eur=nume1+'.'+nume2
 fleur=float(eur)
 if flusdt <= flusd:
@@ -88,5 +74,4 @@ else:
     print(date.strftime(" %d/%m/%Y"), ' EUR ',fleur,' ▼', file=temp)
 
 temp.close()
-
 
