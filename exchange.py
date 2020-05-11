7b28bace34b09e6a302e49aa29695e7c13d21b79
sayman713 <sayman713@yandex.ru>
2020-05-11 03:19:14 +0300

    optimized the code

--- exchange.py
+++ exchange.py
@@ -16,6 +16,7 @@ st = requests.post(lincy).text
 sn = requests.post(lincnow).text
 
 
+
 def str_num(x):
     l = len(x)
     integ = []
@@ -36,38 +37,36 @@ def str_num(x):
     return integ
 
 
+
 # Индексы валют, подставьте нужный индекс, по умолчанию выведено EUR, USD
-#GBP[19:21] AZN[14:16] AUD[9:11] INR[69:71] KZT[74:76]
-#USD[59:61] AMD[24:26] BYN[29:31] BGN[34:36] BRL[39:41]
-#HUF[44:46] HKD[49:51] DKK[54:56] CAD[79:81] KGS[84:86]
-#SNY[89:91] MDL[94:96] NOK[99:101] PLN[104:106] RON[109:111]
-#EUR[64:66] XDR[114:116] SGD[119:121] TJS[124:126] TRY[129:131]
-#TMT[134:136] UZS[139:141] UAH[144:146] CZK[149:151] SEK[154:156]
-#CHF[159:161] ZAR[164:166] KRW[169:171] JPY[174:176]
+# GBP[19:21] AZN[14:16] AUD[9:11] INR[69:71] KZT[74:76]
+# USD[59:61] AMD[24:26] BYN[29:31] BGN[34:36] BRL[39:41]
+# HUF[44:46] HKD[49:51] DKK[54:56] CAD[79:81] KGS[84:86]
+# SNY[89:91] MDL[94:96] NOK[99:101] PLN[104:106] RON[109:111]
+# EUR[64:66] XDR[114:116] SGD[119:121] TJS[124:126] TRY[129:131]
+# TMT[134:136] UZS[139:141] UAH[144:146] CZK[149:151] SEK[154:156]
+# CHF[159:161] ZAR[164:166] KRW[169:171] JPY[174:176]
 
 
 numu1, numu2 = str_num(st)[59:61]
-usd=numu1+'.'+numu2
-flusdt=float(usd)
-
+flusdt = float(numu1+'.'+numu2)
 nume1, nume2 = str_num(st)[64:66]
-eur=nume1+'.'+nume2
-fleurt=float(eur)
+fleurt = float(nume1+'.'+nume2)
 
 
 temp = open('temp', 'w')
 
 numu1, numu2 = str_num(sn)[59:61]
-usd=numu1+'.'+numu2
-flusd=float(usd)
+flusd = float(numu1+'.'+numu2)
 if flusdt <= flusd:
     print(date.strftime(" %d/%m/%Y"), ' USD ',flusd,' ▲', file=temp)
 else:
     print(date.strftime(" %d/%m/%Y"), ' USD ',flusd,' ▼', file=temp)
 
+
+
 nume1, nume2 = str_num(sn)[64:66]
-eur=nume1+'.'+nume2
-fleur=float(eur)
+fleur = float(nume1+'.'+nume2)
 if flusdt <= flusd:
     print(date.strftime(" %d/%m/%Y"), ' EUR ',fleur,' ▲', file=temp)
 else:
